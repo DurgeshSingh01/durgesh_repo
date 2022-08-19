37008f57a3d2c3a6228ed06c8ab999b48ae43969
DurgeshSingh01 <95412673+DurgeshSingh01@users.noreply.github.com>
2022-08-19 11:11:01 -0700

    18th handson

---
+++ AdditionalProblem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_handson
+{
+    internal class AdditionalProblem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the user name");
+            string str = Console.ReadLine();
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("Enter a valid user name");
+            }
+            else
+                Console.WriteLine("Hello " + str);
+        }
+    }
+}
