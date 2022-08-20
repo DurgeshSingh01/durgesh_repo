72856f13401eb5f555b70ee22243fae94baa8bc3
DurgeshSingh01 <95412673+DurgeshSingh01@users.noreply.github.com>
2022-08-20 08:30:09 -0700

    19th_handson

--- Program.cs
+++ Program.cs
@@ -4,22 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2nd_day_demo
+namespace _19th_handson
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int i;
-            float radius;
-            float pi = 3.14f;
-            Console.WriteLine("enter radius");
-            radius = Convert.ToSingle(Console.ReadLine());
-
-            double area = pi * radius * radius;
-            
-            Console.WriteLine(area);
-            Console.WriteLine();
+            Console.WriteLine("Hello");
         }
     }
 }
