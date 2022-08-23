424bc4f66de977d4de77e23869570f29b3feab10
DurgeshSingh01 <95412673+DurgeshSingh01@users.noreply.github.com>
2022-08-23 11:00:37 +0530

    22th_hand-on done in session

---
+++ 22th_Hands-on/Problem11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22th_hands_on
+{
+    internal class Problem11
+    {
+        static void Main(string[] args)
+        {
+            SortedList s1 = new SortedList();
+            s1.Add(3, "Durgesh");
+            s1.Add(2, "Archit");
+            s1.Add(1, "Harshit");
+            s1.Add(4, "Divya");
+            foreach (DictionaryEntry item in s1)
+            {
+                Console.WriteLine(item.Key + "  " + item.Value);
+            }
+        }
+    }
+}
