b44b054bf2aea3951ceec0cc9ac0b59587cc0c2c
DurgeshSingh01 <95412673+DurgeshSingh01@users.noreply.github.com>
2022-08-24 22:50:28 +0530

    ReusableCodeLiberary - Class

---
+++ 24_Hands_on/CreateFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class CreateFile
+    {
+        public static void FirstFile()
+        {
+            string path = @"D:\Mphasis\c#_exercise\MyApp\FirstFile.txt";
+            File.Create(path);
+        }
+    }
+}
