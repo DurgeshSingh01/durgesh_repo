cd348c296f25116ef1ccca4dfe5f7768f4c33949
DurgeshSingh01 <95412673+DurgeshSingh01@users.noreply.github.com>
2022-08-26 13:11:16 +0530

    Console File depend on HRlibrary

---
+++ 26th_Practice/DataStructureFirstDay/Linkedlist1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureFirstday
+{
+    internal class Linkedlist1
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> mylist = new LinkedList<string>();
+            mylist.AddFirst("Durgesh");
+            mylist.AddLast("Aditya");
+            mylist.AddAfter(mylist.First, "Archit");
+            mylist.AddBefore(mylist.Last, "Harshit");
+            LinkedListNode<string> node = mylist.Find("Harshit");
+            node.Value = "Divya";
+            foreach (var item in mylist)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 26th_Practice/DataStructureFirstDay/ObjectListDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLibrary;
+namespace DataStructureFirstday
+{
+    internal class ObjectListDemo
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> emplist = new List<Employee>();
+            emplist.Add(new Employee { EmpID=14,EmpName="Divya",DeptNo=102,City="Mumbai"});
+
+            Employee emp1 = new Employee();
+            emp1.EmpID = 14;
+            emp1.EmpName = "Aditya";
+            emp1.DeptNo = 104;
+            emp1.City = "Bangalore";
+            emp1.CalculateSalary(100000);
+            Console.WriteLine("Gross salary: {0}",emp1.GrossSalary);
+            emplist.Add(emp1);
+            foreach (Employee emp in emplist)
+            {
+                Console.WriteLine(emp.EmpID);
+                Console.WriteLine(emp.EmpName);
+                Console.WriteLine(emp.DeptNo);
+                Console.WriteLine(emp.City);
+            }
+            Console.ReadLine();
+        }
+    }
+}
