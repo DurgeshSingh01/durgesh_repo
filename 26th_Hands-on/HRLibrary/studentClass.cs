d0328d47b149b4733120878788bad7a90f1831e6
DurgeshSingh01 <95412673+DurgeshSingh01@users.noreply.github.com>
2022-08-30 00:08:09 +0530

    HRLibrary

---
+++ 26th_Hands-on/HRLibrary/studentClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HRLibrary
+{
+    public class studentClass
+    {
+        public int _rollno;
+        public int Rollno
+        {
+            get
+            {
+
+                return _rollno;
+            }
+            set
+            {
+                _rollno = value;
+
+            }
+        }
+
+        public string _studname;
+        public string StudentName
+        {
+            set
+            {
+                _studname = value;
+            }
+            get
+            {
+
+                return _studname;
+
+            }
+
+        }
+
+        public string _branchname;
+
+        public string BranchName
+        {
+            get { return _branchname; }
+            set { _branchname = value; }
+        }
+
+    }
+}
