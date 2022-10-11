6c11d1e56f528eb22bbe7ed324ef926c86f421ab
Durgesh Singh <95412673+DurgeshSingh01@users.noreply.github.com>
2022-10-11 16:24:17 +0530

    Add files via upload

---
+++ SQL_Server_19th-22th_sept/21th_sept/21th _assignment.sql
@@ -0,0 +1,13 @@
+
+use Library
+select * from Member
+select * from Issue
+select * from Book
+
+--1)List the various categories and count of books in each category.
+select distinct Category, count(Book_No) from Book group by Category
+
+-- 2)List the book_No and the number of times the book is issued in the descending order of count.
+select b.Book_No ,count(i.member_id) from Book b inner join Issue i
+on b.Book_No = i.Book_No
+group by B.Book_No
