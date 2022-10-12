aa68f7e588901aea1f1773a56440c8185448f07b
Durgesh Singh <95412673+DurgeshSingh01@users.noreply.github.com>
2022-10-12 16:33:54 +0530

    Add files via upload

---
+++ 11th_oct_servicelifeTime/Models/ITransientService.cs
@@ -0,0 +1,18 @@
+﻿namespace ServiceLifeTime.Models
+{
+    public interface ITransientService
+    {
+        Guid GetOperationID();
+
+    }
+
+    public interface IScopedService
+    {
+        Guid GetOperationID();
+    }
+    public interface ISingletonService
+    {
+        Guid GetOperationID();
+
+    }
+}
---
+++ 11th_oct_servicelifeTime/Models/OperationServices.cs
@@ -0,0 +1,18 @@
+﻿namespace ServiceLifeTime.Models
+{
+    public class OperationServices : ISingletonService, ITransientService, IScopedService
+    {
+        Guid id;
+
+        public OperationServices()
+        {
+            id = Guid.NewGuid();
+        }
+
+        public Guid GetOperationID()
+        {
+            //throw new NotImplementedException();
+            return id;
+        }
+    }
+}
